52df2c9ce72abfbad060fd205cb6864454ce4cd0
Tony Dang <tonydang0124@gmail.com>
2020-09-19 15:10:53 -1000

    Updates

---
+++ precache-manifest.f0b23bcad773237bc8f7e642b3ca94e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73119c399f7195875fbe06bce49dde4d",
+    "url": "/Youth-Finance-Student-Loan/index.html"
+  },
+  {
+    "revision": "32affef18147681a3d67",
+    "url": "/Youth-Finance-Student-Loan/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e098b0765968bac33254",
+    "url": "/Youth-Finance-Student-Loan/static/css/main.62441181.chunk.css"
+  },
+  {
+    "revision": "32affef18147681a3d67",
+    "url": "/Youth-Finance-Student-Loan/static/js/2.13222244.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Youth-Finance-Student-Loan/static/js/2.13222244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e098b0765968bac33254",
+    "url": "/Youth-Finance-Student-Loan/static/js/main.ad539c66.chunk.js"
+  },
+  {
+    "revision": "bd421f4e6b2c902cf481",
+    "url": "/Youth-Finance-Student-Loan/static/js/runtime-main.49fd5d4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Youth-Finance-Student-Loan/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5467d7c8f910b7229c2f04c0b5fc79f7",
+    "url": "/Youth-Finance-Student-Loan/static/media/titlebutton.5467d7c8.png"
+  },
+  {
+    "revision": "51468ce6793b39aa5b83da3aaa1f4efb",
+    "url": "/Youth-Finance-Student-Loan/static/media/titlepage.51468ce6.png"
+  }
+]);
